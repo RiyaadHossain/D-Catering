5c013366f879d6ecd9416f0e63651a7f41ea531e
RiyaadHossain <riyadhossain.dev@gmail.com>
2023-09-02 18:11:11 +0600

    Footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
 
 export default function Footer() {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <footer className="footer">
+            <div className="left-section">
+                {/* <div className="logo"> */}
+                    <FastfoodIcon size="large" />
+                {/* </div> */}
+                <div className="footer-message">
+                    <p>&copy; {new Date().getFullYear()} Dhaka Catering</p>
+                </div>
+            </div>
+            <div className="right-section">
+                <ul className="social-icons">
+                    <li><a className='link' href="#"><FacebookOutlinedIcon /></a></li>
+                    <li><a className='link' href="#"><InstagramIcon /></a></li>
+                    <li><a className='link' href="#"><YouTubeIcon /></a></li>
+                </ul>
+            </div>
+        </footer>
+    )
 }
